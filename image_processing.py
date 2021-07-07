27d8e11b77726dc38ae98ad175f2579b6684540d
ndarin <tendaimoyo17@gmail.com>
2021-07-07 17:46:25 +0200

    Solving save 'None' error by running im.thumbnail(size) before im.rotate(90).

--- image_processing.py
+++ image_processing.py
@@ -8,6 +8,6 @@ size = 128, 128
 for infile in glob.glob("*.tiff"):
   file, ext = os.path.splitext(infile)
   with Image.open(infile) as im:
+    im.thumbnail(size)
     out = im.rotate(90)
-    out = out.thumbnail(size)
-    out.save(file, "JPEG")
+    out.save(file + ".jpg", "JPEG")
