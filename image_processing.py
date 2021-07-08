2f54068fc718597b91c9215fbf2ad4191cf3fa29
ndarin <tendaimoyo17@gmail.com>
2021-07-08 15:24:39 +0200

    Altering the script to run on Linux systems.
    Adding 'if statement' to change image mode to 'RGB' to solve IOError,
    when saving to JPEG.
    
    Adding 'sys.argv' to access global variables for image file and,
    destination directory. Allows script to be reusable.
    
    Adding comments to the script.

--- image_processing.py
+++ image_processing.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python3
 
 from PIL import Image
-import os, glob
+import os, glob, sys
 
 size = 128, 128
-
-for infile in glob.glob("*.tiff"):
-  file, ext = os.path.splitext(infile)
+path = sys.argv[1]
+# first obtain a list of .tiff images from the images directory
+for infile in glob.glob(path + '/*.tiff'):
+  # remove 'image/' and '.tiff' from infile variable and save file name
+  lane, _ = os.path.splitext(infile)
+  _, file = os.path.split(lane)
+  # open the image file
   with Image.open(infile) as im:
+    # change image mode to 'RGB', necessary to convert to JPEG
+    if im.mode != 'RGB':
+        im = im.convert('RGB')
+    # convert image to thumbnail
     im.thumbnail(size)
-    out = im.rotate(90)
-    out.save(file + ".jpg", "JPEG")
+    # rotate image and save output
+    out = im.rotate(-90)
+    # add the destination directory path '/opt/icon/' and save image
+    out.save(sys.argv[2] + file + "JPEG")
