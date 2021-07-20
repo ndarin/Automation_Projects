a1abddd3d47eae8df64e3577b6b2d06370eb5f87
ndarin <tendaimoyo17@gmail.com>
2021-07-20 13:12:46 +0200

    Adding a new function 'generate_no_attachment' that creates emails without attachments.

--- emails.py
+++ emails.py
@@ -27,6 +27,16 @@ def generate(sender, recipient, subject, body, attachment_path):
 
   return message
 
+def generate_no_attachment(sender, recipient, subject, body):
+    """Creates an email without an attachement."""
+    # Basic Email formatting
+    message = email.message.EmailMessage()
+    message["From"] = sender
+    message["To"] = recipient
+    message["Subject"] = subject
+    message.set_content(body)
+    return message
+
 def send(message):
   """Sends the message to the configured SMTP server."""
   mail_server = smtplib.SMTP('localhost')
